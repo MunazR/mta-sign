473fb7cc943681ff47f80708f824e6bc708268b5
Munaz Rahman <rahm.muna@gmail.com>
2022-01-29 19:43:49 -0500

    Wrap up API endpoint

--- data/stations.js
+++ data/stations.js
@@ -5,7 +5,7 @@ const parse = require('csv-parse/sync').parse;
 const file = fs.readFileSync(path.resolve(__dirname, 'stations.csv'));
 const data = parse(file.toString(), { columns: true, skip_empty_lines: true });
 
-const getStationData = (stopId) => data.filter((station) => station['GTFS Stop ID'] === stopId);
+const getStationData = (stopId) => data.find((station) => station['GTFS Stop ID'] === stopId);
 
 module.exports = {
   getStationData,
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,13 @@ const { getStationData } = require("../data/stations");
 const nyctSubwayProtoRoot = protobuf.loadSync(path.join(__dirname, '../proto/nyct-subway.proto'));
 const FeedMessage = nyctSubwayProtoRoot.lookupType("transit_realtime.FeedMessage");
 
+const stationIds = process.env.GTFS_STOPS.split(',');
+const stationData = stationIds
+  .map(getStationData)
+  .reduce((result, station) => ({ ...result, [station['GTFS Stop ID']]: station, }), {});
+
+const feeds = process.env.FEEDS.split(',');
+
 const router = express.Router();
 
 router.use((req, res, next) => {
@@ -15,35 +22,77 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get('/', async (req, res) => {
+router.get('/data', async (req, res) => {
   try {
-    const response = await axios({
-      method: 'get',
-      url: 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm',
-      responseType: 'arraybuffer',
-      headers: {
-        'x-api-key': process.env.MTA_API_KEY,
-      },
-    });
+    const updatesForStation = await Promise.all(feeds.map(async (feed) => {
+      const response = await axios({
+        method: 'get',
+        url: `https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-${feed}`,
+        responseType: 'arraybuffer',
+        headers: {
+          'x-api-key': process.env.MTA_API_KEY,
+        },
+      });
 
-    const decoded = FeedMessage.decode(Buffer.from(response.data));
-    const object = FeedMessage.toObject(decoded, {
-      longs: String,
-      enums: String,
-      bytes: String,
-    });
+      const feedMessage = FeedMessage.toObject(
+        FeedMessage.decode(Buffer.from(response.data)),
+        {
+          longs: String,
+          enums: String,
+          bytes: String,
+        },
+      );
 
-    console.log(getStationData('M18'))
+      return feedMessage.entity
+        .map((entity) => {
+          const { tripUpdate, id } = entity;
+          if (!tripUpdate) {
+            return;
+          }
+
+          const { stopTimeUpdate, trip } = tripUpdate;
+          if (!stopTimeUpdate || !trip) {
+            return;
+          }
+
+          const stop = stopTimeUpdate
+            .find((update) => stationIds.some((id) => id === update.stopId.substring(0, 3)));
+
+          if (!stop) {
+            return;
+          }
+
+          return {
+            id,
+            arrivalTime: stop.arrival.time,
+            stopId: stop.stopId,
+            routeId: trip.routeId,
+          };
+        })
+        .filter((update) => Boolean(update))
+    }));
+
+    const stopTimeUpdates = updatesForStation
+      .flat()
+      .sort((update1, update2) => update1.arrivalTime - update2.arrivalTime)
+      .reduce((result, update) => {
+        const stopId = update.stopId;
+        return {
+          ...result,
+          [stopId]: [
+            ...(result[stopId] ?? []),
+            update,
+          ],
+        };
+      }, {});
+
+    return res.json({
+      stations: stationData,
+      stopTimeUpdates,
+    });
   } catch (error) {
-    console.log("Error processing request", error);
-    return res.status(400).send({ error });
+    return res.status(500).send({ error });
   }
-
-  res.send('home page');
-});
-
-router.get('/about', (req, res) => {
-  res.send('About us');
 });
 
 module.exports = router;
